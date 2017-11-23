f7068f34f1cae3e5601dc2c9d083b8189729fc05
kevingilfether <kevingilfether@gmail.com>
2017-11-23 10:33:04 -0500

    added board state constructor

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Board
+    {
+        private SquareState[,] squareState;
+        public SquareState NextTurn { get; private set;}
+
+        public Board()
+        {
+            squareState = new SquareState[3, 3];
+            NextTurn = SquareState.X;
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -11,6 +11,7 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
