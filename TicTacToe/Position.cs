16d1f21c072618840d7659e073c79f5e593c2408
kevingilfether <kevingilfether@gmail.com>
2017-11-23 10:10:24 -0500

    Added position class, properties

---
+++ TicTacToe/Position.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Position
+    {
+        //fields
+        private int row;
+        private int column;
+
+        //Properties
+        public int Row
+        {
+            get { return this.row; }
+            set { this.row = value; }
+        }
+
+        public int Column
+        {
+            get { return this.column; }
+            set { this.column = value; }
+        }
+
+
+    }
+}
